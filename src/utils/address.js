7d82e257af72e34a4efe80d066948206a569dbc0
Kenzo <presto12321@gmail.com>
2018-10-31 13:12:42 -0400

    change setInterval to setTimeout as per toshi wallet docs

--- src/utils/address.js
+++ src/utils/address.js
@@ -4,39 +4,46 @@ import {
 
 export let address = typeof web3 !== 'undefined' ? web3.eth.accounts[0] : ''; // eslint-disable-line no-undef
 
-setInterval(() => {
-  const currentAddress = typeof web3 !== 'undefined' ? web3.eth.accounts[0] : ''; // eslint-disable-line no-undef
-  // Logged out
-  if (currentAddress !== address && currentAddress === undefined && store.getState().threeBox.isLoggedIn) {
-    store.dispatch({
-      type: 'SHOW_LOGGEDOUT_MODAL',
-      loggedOutModal: true,
-    });
-  }
+const pollNetworkAndAddress = () => {
+  setTimeout(() => {
+    const currentAddress = typeof web3 !== 'undefined' ? web3.eth.accounts[0] : ''; // eslint-disable-line no-undef
+    // Logged out
+    if (currentAddress !== address && currentAddress === undefined && store.getState().threeBox.isLoggedIn) {
+      store.dispatch({
+        type: 'SHOW_LOGGEDOUT_MODAL',
+        loggedOutModal: true,
+      });
+    }
 
-  // Switched address
-  if (currentAddress !== address && typeof currentAddress === 'string' && address !== undefined && store.getState().threeBox.isLoggedIn) {
-    store.dispatch({
-      type: 'SHOW_SWITCHED_ADDRESS_MODAL',
-      switchedAddressModal: true,
-      switched: true,
-    });
-  }
+    // Switched address
+    if (currentAddress !== address && typeof currentAddress === 'string' && address !== undefined && store.getState().threeBox.isLoggedIn) {
+      store.dispatch({
+        type: 'SHOW_SWITCHED_ADDRESS_MODAL',
+        switchedAddressModal: true,
+        switched: true,
+      });
+    }
 
-  // Logged in to MM
-  // if (currentAddress !== address && typeof currentAddress === 'string' && address === undefined) {
-  //   // window.location.reload();
-  //   store.dispatch({
-  //     type: 'HANDLE_WALLET_LOGIN_DETECTED_MODAL',
-  //     loginDetectedModal: true,
-  //   });
-  //   // web3 login detected
-  //   // show modal
-  //   // reload page from modal
-  // }
+    // Logged in to MM
+    // if (currentAddress !== address && typeof currentAddress === 'string' && address === undefined) {
+    //   // window.location.reload();
+    //   store.dispatch({
+    //     type: 'HANDLE_WALLET_LOGIN_DETECTED_MODAL',
+    //     loginDetectedModal: true,
+    //   });
+    //   // web3 login detected
+    //   // show modal
+    //   // reload page from modal
+    // }
 
-  address = currentAddress;
-}, 1500);
+    console.log('polling');
+
+    address = currentAddress;
+    pollNetworkAndAddress();
+  }, 1000);
+};
+
+pollNetworkAndAddress();
 
 export {
   address as
