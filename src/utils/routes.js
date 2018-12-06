c873f703e50114c59eff0b430ee156ca1a221a02
Kenzo <presto12321@gmail.com>
2018-12-06 16:28:24 -0500

    /EditProfile => /edit

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,7 +1,7 @@
 export const ABOUT = '/about';
 export const LANDING = '/';
 export const PROFILE = '/profile';
-export const EDITPROFILE = '/editprofile';
+export const EDITPROFILE = '/edit';
 export const PRIVACY = '/privacy';
 export const TERMS = '/terms';
 export const JOBS = '/jobs';
