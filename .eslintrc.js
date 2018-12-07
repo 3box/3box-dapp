178a3e1316c27a4666e5af2e891a7f4253ec516d
Kenzo <presto12321@gmail.com>
2018-12-07 15:29:14 -0500

    handle eslint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-    "extends": "airbnb",
-    "parser": "babel-eslint"
-};
+    "extends": ["react-app", "airbnb", "plugin:jsx-a11y/recommended"],
+    "plugins": ["jsx-a11y"]
+}
