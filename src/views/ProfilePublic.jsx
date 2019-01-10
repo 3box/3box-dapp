b460c9baae2e7cae1f5e526e9b2b721320ce8867
Kenzo <presto12321@gmail.com>
2019-01-10 10:22:16 -0500

    improve create profile page responsiveness

---
+++ src/views/ProfilePublic.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Feed from '../components/Feed.jsx';
+import ProfileDetails from '../components/ProfileDetails.jsx';
+import Nav from '../components/Nav.jsx';
+import './styles/Profile.css';
+
+const ProfilePublic = () => (
+  <div>
+    <Nav />
+    <div id="profile__page">
+      <div id="profile__contents">
+        <ProfileDetails />
+        <Feed />
+      </div>
+    </div>
+  </div>
+);
+
+export default ProfilePublic;
