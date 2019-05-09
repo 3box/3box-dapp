9965e0f095ae98b6d4e5743ac607331473f8a662
Kenzo <presto12321@gmail.com>
2019-05-09 10:20:43 -0400

    in dapp logo black

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link, withRouter, NavLink } from 'react-router-dom';
 
-import ThreeBoxLogo from '../assets/ThreeBoxLogoBlue.svg';
+import ThreeBoxLogo from '../assets/ThreeBoxLogoBlack.svg';
 import ThreeBoxB from '../assets/3Box3Blue.svg';
 import DropDown from '../assets/DropDown.svg';
 import actions from '../state/actions';
