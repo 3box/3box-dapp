293557f1e1c325e19509519bf917c539e3076e1c
Kenzo <presto12321@gmail.com>
2019-05-10 19:40:59 -0400

    paginate collectibles fetching on scroll

--- src/state/actions/profile/getCollectibles2.js
+++ src/state/actions/profile/getCollectibles2.js
@@ -4,9 +4,36 @@ import {
 
 const getCollectibles = (address, onPublicProfile) => async (dispatch) => {
   try {
-    const res = await fetch(`https://api.opensea.io/api/v1/assets?owner=${address}&order_by=current_price&order_direction=asc&limit=100&offset=0`);
+    let offset = 0;
+    let collection = [];
+
+    const checkAndGetMoreCollectibles = async (collectiblesArray) => {
+      console.log('incount', collectiblesArray.length);
+      if (collectiblesArray.length >= 100) {
+        offset += 100;
+        console.log('thisoffset', offset);
+        const collectiblesRes = await fetch(`https://api.opensea.io/api/v1/assets?owner=${'0x68b42e44079d1d0a4a037e8c6ecd62c48967e69f'}&order_by=current_price&order_direction=asc&offset=${offset}&limit=100`);
+        const collectiblesData = await collectiblesRes.json();
+        console.log('secondcollectiblesData', collectiblesData);
+        collection.push(collectiblesData.assets);
+        if (offset === 700) return;
+        checkAndGetMoreCollectibles(collectiblesData.assets);
+      }
+    };
+
+    const res = await fetch(`https://api.opensea.io/api/v1/assets?owner=${'0x68b42e44079d1d0a4a037e8c6ecd62c48967e69f'}&order_by=current_price&order_direction=asc&offset=0&limit=100`);
+    // const res = await fetch(`https://api.opensea.io/api/v1/assets?owner=${address}&order_by=current_price&order_direction=asc`);
     const data = await res.json();
-    let collection = data.assets;
+    console.log('collectiblesData', data);
+
+    // const res2 = await fetch(`https://api.opensea.io/api/v1/assets?owner=${'0x68b42e44079d1d0a4a037e8c6ecd62c48967e69f'}&order_by=current_price&order_direction=asc&offset=95&limit=100`);
+    // // const res = await fetch(`https://api.opensea.io/api/v1/assets?owner=${address}&order_by=current_price&order_direction=asc`);
+    // const data2 = await res2.json();
+    // console.log('collectiblesData2', data2);
+    // collection.push(data.assets);
+    checkAndGetMoreCollectibles(data.assets);
+
+    console.log('totalCollectibles', collection);
 
     const collectiblesFavoritesToRender = [];
     let updatedCollectiblesFavorites = [];
