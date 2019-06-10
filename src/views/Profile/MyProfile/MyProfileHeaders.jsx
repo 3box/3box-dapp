65e1592f9e3a96e4fd39955731f49385f6b3be3e
Kenzo <presto12321@gmail.com>
2019-06-10 10:57:02 -0400

    metatags for profiles with no name

--- src/views/Profile/MyProfile/MyProfileHeaders.jsx
+++ src/views/Profile/MyProfile/MyProfileHeaders.jsx
@@ -7,17 +7,17 @@ const MyProfileHeaders = ({
   currentAddress,
 }) => (
     <Helmet>
-      <title>{name}</title>
+      <title>{name || '3Box | Ethereum Profiles'}</title>
       <meta name="description" content={`3Box Profile for ${currentAddress}`} />
 
       <meta property="og:description" content={`3Box Profile for ${currentAddress}`} />
       <meta property="og:url" content={`https://3box.io/${currentAddress}`} />
-      <meta property="og:title" content={name} />
+      <meta property="og:title" content={name || '3Box | Ethereum Profiles'} />
       <meta property="og:image" content={`${image ? `https://ipfs.infura.io/ipfs/${image[0].contentUrl['/']}` : ''}`} />
 
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:site" content="@3boxdb" />
-      <meta name="twitter:title" content={name} />
+      <meta name="twitter:title" content={name || '3Box | Ethereum Profiles'} />
       <meta name="twitter:description" content={`3Box Profile for ${currentAddress}`} />
       <meta name="twitter:image" content={`${image ? `https://ipfs.infura.io/ipfs/${image[0].contentUrl['/']}` : ''}`} />
     </Helmet>
