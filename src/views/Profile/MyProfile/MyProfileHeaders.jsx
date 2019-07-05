07799ae5f0aa78218e0f38833652cb1c266e5819
Kenzo <presto12321@gmail.com>
2019-07-05 18:44:32 -0400

    revert back to default tw image

--- src/views/Profile/MyProfile/MyProfileHeaders.jsx
+++ src/views/Profile/MyProfile/MyProfileHeaders.jsx
@@ -1,12 +1,5 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import image2base64 from 'image-to-base64';
-
-const getDataUri = async (url) => {
-  const uri = await image2base64(url);
-  console.log('ingetdatauri', uri);
-  return uri;
-};
 
 const MyProfileHeaders = ({
   image,
@@ -14,20 +7,19 @@ const MyProfileHeaders = ({
   currentAddress,
 }) => (
     <Helmet>
-      {console.log('objectimage', image)}
       <title>{name || '3Box | Ethereum Profiles'}</title>
       <meta name="description" content={`3Box Profile for ${currentAddress}`} />
 
       <meta property="og:description" content={`3Box Profile for ${currentAddress}`} />
       <meta property="og:url" content={`https://3box.io/${currentAddress}`} />
       <meta property="og:title" content={name || '3Box | Ethereum Profiles'} />
-      <meta property="og:image" content={`${(image && image[0]) ? getDataUri(`https://ipfs.infura.io/ipfs/${image[0].contentUrl['/']}`) : ''}`} />
+      <meta property="og:image" content={`${(image && image[0]) ? `https://ipfs.infura.io/ipfs/${image[0].contentUrl['/']}` : ''}`} />
 
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:site" content="@3boxdb" />
       <meta name="twitter:title" content={name || '3Box | Ethereum Profiles'} />
       <meta name="twitter:description" content={`3Box Profile for ${currentAddress}`} />
-      <meta name="twitter:image" content={`${(image && image[0]) ? getDataUri(`https://ipfs.infura.io/ipfs/${image[0].contentUrl['/']}`) : ''}`} />
+      <meta name="twitter:image" content={`${(image && image[0]) ? `https://ipfs.infura.io/ipfs/${image[0].contentUrl['/']}` : ''}`} />
     </Helmet>
   );
 
