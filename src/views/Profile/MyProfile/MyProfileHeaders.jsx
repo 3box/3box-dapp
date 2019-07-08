afc37ff7ad3dfcb53ef098f0eb7302fcf32d046c
Kenzo <presto12321@gmail.com>
2019-07-08 10:19:58 -0400

    add default twitter preview

--- src/views/Profile/MyProfile/MyProfileHeaders.jsx
+++ src/views/Profile/MyProfile/MyProfileHeaders.jsx
@@ -19,7 +19,7 @@ const MyProfileHeaders = ({
       <meta name="twitter:site" content="@3boxdb" />
       <meta name="twitter:title" content={name || '3Box | Ethereum Profiles'} />
       <meta name="twitter:description" content={`3Box Profile for ${currentAddress}`} />
-      <meta name="twitter:image" content={`${(image && image[0]) ? `https://ipfs.infura.io/ipfs/${image[0].contentUrl['/']}` : ''}`} />
+      <meta name="twitter:image" content="https://i.imgur.com/9YTMIoE.png" />
     </Helmet>
   );
 
