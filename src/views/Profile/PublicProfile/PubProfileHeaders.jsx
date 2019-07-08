b29c1775ecf9c05d262e0d284a29ad251ea12e42
Kenzo <presto12321@gmail.com>
2019-07-08 17:57:55 -0400

    test dynamic but w/o imgs

--- src/views/Profile/PublicProfile/PubProfileHeaders.jsx
+++ src/views/Profile/PublicProfile/PubProfileHeaders.jsx
@@ -15,7 +15,7 @@ const PubProfileHeaders = ({
       <meta property="og:description" content={`Profile for ${shortenEthAddr(otherProfileAddress)}`} />
       <meta property="og:url" content={`https://test.3box.io/${otherProfileAddress}`} />
       <meta property="og:title" content={`${otherName} on 3Box` || '3Box | Ethereum Profiles'} />
-      <meta property="og:image" content={`${(otherImage && otherImage[0]) ? `https://ipfs.infura.io/ipfs/${otherImage[0].contentUrl['/']}` : ''}`} />
+      {/* <meta property="og:image" content={`${(otherImage && otherImage[0]) ? `https://ipfs.infura.io/ipfs/${otherImage[0].contentUrl['/']}` : ''}`} /> */}
 
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:site" content="@3boxdb" />
