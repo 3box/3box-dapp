de32af21c6e8613a8fb8fb24bf9de8d7ad1c1007
Kenzo <presto12321@gmail.com>
2019-07-11 12:00:38 -0400

    handle emtpy names

--- src/views/Profile/PublicProfile/TwitterHeader.jsx
+++ src/views/Profile/PublicProfile/TwitterHeader.jsx
@@ -8,12 +8,12 @@ const TwitterHeader = ({
   otherProfileAddress,
 }) => (
     <Helmet>
-      <title>{`${otherName} on 3Box` || '3Box | Ethereum Profiles'}</title>
+      <title>{otherName ? `${otherName} on 3Box` : '3Box | Ethereum Profiles'}</title>
       <meta name="description" content={`Profile for ${shortenEthAddr(otherProfileAddress)}`} />
 
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:site" content="@3boxdb" />
-      <meta name="twitter:title" content={`${otherName} on 3Box` || '3Box | Ethereum Profiles'} />
+      <meta name="twitter:title" content={otherName ? `${otherName} on 3Box` : '3Box | Ethereum Profiles'} />
       <meta name="twitter:description" content={`Profile for ${shortenEthAddr(otherProfileAddress)}`} />
       <meta name="twitter:image" content="https://i.imgur.com/RXJO8FD.png" />
     </Helmet>
