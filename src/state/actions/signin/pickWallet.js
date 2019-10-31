bdfe8cb9c587c734c54c865158dfa295da739aa5
Kenzo <presto12321@gmail.com>
2019-10-31 19:09:15 -0400

    master build failing

--- src/state/actions/signin/pickWallet.js
+++ src/state/actions/signin/pickWallet.js
@@ -2,8 +2,8 @@ import Web3Connect from 'web3connect';
 import WalletConnectProvider from '@walletconnect/web3-provider';
 import Portis from '@portis/web3';
 import Fortmatic from 'fortmatic';
-import Squarelink from 'squarelink';
-import Torus from '@toruslabs/torus-embed';
+// import Squarelink from 'squarelink';
+// import Torus from '@toruslabs/torus-embed';
 
 import history from '../../../utils/history';
 import connectProviderToDapp from './connectProviderToDapp';
