9f344f69409fa0d13034927ef339723999c32922
Jason Smythe <jason@smythemail.za.net>
2020-01-14 11:25:58 +0100

    Update year 2018 -> 2020

--- src/views/Profile/SideBar.jsx
+++ src/views/Profile/SideBar.jsx
@@ -166,7 +166,7 @@ const SideBar = ({
 
             <div id="profile__footer">
               <div id="profile__footer__contents">
-                <p>3Box 2018</p>
+                <p>3Box 2020</p>
                 <Link to={routes.TERMS}>Terms</Link>
                 <Link to={routes.PRIVACY}>Privacy</Link>
               </div>
